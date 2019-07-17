6c19c4800fe76821e8c7b6f2121140759135c046
Chris <chris.m.hein@gmail.com>
2019-07-17 18:09:56 -0400

    replaced functions with arrow functions, renamed variable, removed unused code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,28 +2,10 @@ require("dotenv").config();
 
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-const Table = require('cli-table3');
 const connection = require('./sql')
 const log = console.log;
 
-function displayItems() {
-    let myQuery = 'SELECT item_id, product, CONCAT ("$", price) AS price FROM products'
-    connection.query(myQuery, function (error, results) {
-        if (error) throw error;
-        log(chalk.blue.bold('\nHere\'s what\'s for sale:\n'));
-        let table = new Table({
-            head: ['ID', 'Product', 'Price'],
-            colWidths: [10, 20, 10]
-        });
-        results.forEach(el => {
-            table.push([`${chalk.blue([el['item_id']])}`, `${chalk.yellow([el['product']])}`, `${chalk.green([el['price']])}`])
-        });
-        log(table.toString())
-        customerInput();
-    });
-}
-
-function customerInput(arraything) {
+customerInput = (itemArray) => {
     inquirer
         .prompt([
             {
@@ -31,7 +13,7 @@ function customerInput(arraything) {
                 message: "Please select the product you would like to purchase",
                 type: "list",
                 choices: function () {
-                    return arraything.map(function (el) {
+                    return itemArray.map(function (el) {
                         return `${el.item_id} || ${el.product} || ${el.price}`
                     })
                 }
@@ -54,7 +36,7 @@ function customerInput(arraything) {
         });
 };
 
-function checkInventory(item, quantity) {
+checkInventory = (item, quantity) => {
     connection.query('SELECT stock_quantity, round(price,2) as price, product_sales FROM products WHERE item_id = ?', [item], function (error, results) {
         if (error) throw error;
         let currentSales = results[0]["product_sales"]
@@ -74,7 +56,7 @@ function checkInventory(item, quantity) {
     });
 };
 
-function start() {
+start = () => {
     inquirer
         .prompt([
             {
@@ -86,7 +68,6 @@ function start() {
         ])
         .then(answers => {
             if (answers.doWhat === "Show me what's for sale") {
-                //displayItems();
                 showItems();
             } else {
                 connection.end();
@@ -95,12 +76,12 @@ function start() {
         });
 }
 
-function showItems() {
+showItems = () => {
     let myQuery = 'SELECT item_id, product, CONCAT ("$", price) AS price, stock_quantity FROM products WHERE stock_quantity > 0'
     connection.query(myQuery, function (error, results) {
         if (error) throw error;
-        let arraything = results.filter((el) => el.stock_quantity > 0)
-        customerInput(arraything);
+        let inventoryArray = results.filter((el) => el.stock_quantity > 0)
+        customerInput(inventoryArray);
     });
 };
 
